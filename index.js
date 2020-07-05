81d05e261709b4521458a139430da73aa6d46f1b
vs <vs2001dor@gmail.com>
2020-07-05 15:36:06 +0200

    some changes made

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
-var user = new User("Paul");
 
-console.log(user.myfield);
 
 app.get('/home', function(req,  res){
 	res.render('home');
@@ -24,14 +22,7 @@ app.get('/profile/:name', function(req, res){
 	res.render('profile', {name: req.params.name});
 });
 
-var myReader = fs.createReadStream(__dirname +  "/readMe.txt", 'utf8');
 
-myReader.on('data' , function (chunk) {
-	console.log(chunk + " \n my chunk is here");
-});
-
-app.listen({
-	host: 'localhost',
-	port: Port,
-	exclusive: true
+app.listen(Port, function(){
+	console.log("works"); 
 });
